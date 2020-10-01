fe74fb7d63d798fd256ed41420e8cb3b17f6b850
Marko Babic <marko@narrative.io>
2020-10-01 13:42:22 -0700

    Revert "Fixed the encoders using SerializerBuildHelper.createSerializerForUserDefinedType"
    This reverts commit d23a79e425148abcb85cee2c86b2c822e99cfb3c.

--- src/main/scala/magellan/encoders/Encoders.scala
+++ src/main/scala/magellan/encoders/Encoders.scala
@@ -5,7 +5,6 @@ import org.apache.spark.sql.Encoder
 import org.apache.spark.sql.catalyst.analysis.GetColumnByOrdinal
 import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
 import org.apache.spark.sql.catalyst.expressions._
-import org.apache.spark.sql.catalyst._
 import org.apache.spark.sql.types._
 
 import scala.reflect._
@@ -13,26 +12,26 @@ import scala.reflect.runtime.universe
 
 object Encoders {
 
-  implicit def encoderForPoint: Encoder[Point] = {
 
-    val udt =  new PointUDT()
-    val sqlType = udt.sqlType
+  implicit def encoderForPoint: Encoder[Point] = {
+    val sqlType = new PointUDT().sqlType
 
     ExpressionEncoder[Point](
-      objSerializer = SerializerBuildHelper.createSerializerForUserDefinedType(BoundReference(0, ObjectType(classOf[Point]), nullable = true), udt, udt.getClass)
-        ,
-      objDeserializer = MagellanDeserializer(
+      objSerializer = 
+        MagellanSerializer(
+          BoundReference(0, ObjectType(classOf[Point]), nullable = true), sqlType),
+      objDeserializer =
+        MagellanDeserializer(
           GetColumnByOrdinal(0, sqlType), classOf[Point]),
       clsTag = classTag[Point])
   }
 
   implicit def encoderForPolygon: Encoder[Polygon] = {
-    val udt =  new PolygonUDT()
-    val sqlType = udt.sqlType
-
+    val sqlType = new PolygonUDT().sqlType
     ExpressionEncoder[Polygon](
       objSerializer = 
-        SerializerBuildHelper.createSerializerForUserDefinedType(BoundReference(0, ObjectType(classOf[Polygon]), nullable = true), udt, udt.getClass),
+        MagellanSerializer(
+          BoundReference(0, ObjectType(classOf[Polygon]), nullable = true), sqlType),
       objDeserializer =
         MagellanDeserializer(
           GetColumnByOrdinal(0, sqlType), classOf[Polygon]),
@@ -40,12 +39,11 @@ object Encoders {
   }
 
   implicit def encoderForPolyLine: Encoder[PolyLine] = {
-    val udt =  new PolyLineUDT()
-    val sqlType = udt.sqlType
-
+    val sqlType = new PolyLineUDT().sqlType
     ExpressionEncoder[PolyLine](
       objSerializer = 
-        SerializerBuildHelper.createSerializerForUserDefinedType(BoundReference(0, ObjectType(classOf[PolyLine]), nullable = true), udt, udt.getClass),
+        MagellanSerializer(
+          BoundReference(0, ObjectType(classOf[PolyLine]), nullable = true), sqlType),
       objDeserializer =
         MagellanDeserializer(
           GetColumnByOrdinal(0, sqlType), classOf[PolyLine]),
